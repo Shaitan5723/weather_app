86856e46c4e765c296069254ed8751b3a9edf712
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-07-11 19:50:03 +1000

    UV color indicator
    added if statements to change the background color of UV index based on severity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 let searchButton = $("#searchCitiesBtn")
+let cityAll = [];
+
+
+
 
 //runs the API request upon hitting the 'Get Recipes' button
 searchButton.click (function()  {
@@ -17,7 +21,7 @@ $(document).keypress(function (e) {
 
 
 
-// API request and creation of variables for usage with the data. The variables need to be interpolated ${} and inverted commas are needed
+// geolocation API request to return latitudes and longitudes for weather API
 async function sendApiRequest(){
   let API_KEY = "406b266e02e760ad8947072525883d81"
   let query = $("#search-city").val();
@@ -25,12 +29,22 @@ async function sendApiRequest(){
   $.ajax({
     url: queryURL
   })
+
+    // weather API request
     .then(function (response){
       let lat = (response[0].lat)
       let lon = (response[0].lon)
   
-      console.log(response)
-      $("#searchedCity").html(response[0].name);
+      let cityName = response[0].name
+      $("#searchedCity").html(cityName);
+
+      //sets local storage and appends new button to the search history bar
+      cityAll.push(cityName);
+      let searchHistoryBtns = 
+      `<button class="btn btn-secondary" type="button" id="searchedBtn">${cityName}</button>`
+      $("#searchHistory").append(searchHistoryBtns);
+
+      localStorage.setItem("searchHistory", JSON.stringify(cityAll));
 
       let API_KEY = "406b266e02e760ad8947072525883d81"
       let mainQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=alerts&appid=${API_KEY}`
@@ -49,19 +63,32 @@ async function sendApiRequest(){
         $("#currentTemp").html(`Temperature: ${data.current.temp} \u00B0C`)
         $("#currentWind").html(`Wind: ${data.current.wind_speed} m/s`)
         $("#currentHumid").html(`Humidity: ${data.current.humidity}%`)
+        
         $("#currentUV").html(`UV Index: ${data.current.uvi}`)
+        if (data.current.uvi <= 3) {
+          $("#currentUV").css("background-color", "green")
+        }
+        else if (data.current.uvi > 3 && data.current.uvi <= 6) {
+          $("#currentUV").css("background-color", "yellow")
+        }
+        else if (data.current.uvi >= 7) {
+          $("#currentUV").css("background-color", "red")
+        }
 
-        for (var i = 0; i <= 4; i++) { 
+        $(".card-group").empty() //empties out the 5-day forecast cards
+        for (var i = 0; i <= 4; i++) { //5-day forecast cards
           let forecastDates = new Date(data.daily[i].dt*1000).toLocaleDateString("en-us")
+          let iconCode = data.daily[i].weather[0].icon;
+          let iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
           //console.log(forecastDates)
           let cardContent =         
-          `<div class="card col-2" id="">
+          `<div class="card col-2" id="forecast${i}">
             <div class="card-body">
               <h5>${forecastDates}</h5>
-              <img>
-              <h6>Temp: ${data.daily[0].temp.day} \u00B0C</h6>
-              <h6>Wind: ${data.daily[0].wind_speed} m/s</h6>
-              <h6>Humidity: ${data.daily[0].humidity}%</h6>`
+              <img src=${iconUrl}>
+              <h6>Temp: ${data.daily[i].temp.day} \u00B0C</h6>
+              <h6>Wind: ${data.daily[i].wind_speed} m/s</h6>
+              <h6>Humidity: ${data.daily[i].humidity}%</h6>`
         $(".card-group").append(cardContent)
         }
       }
